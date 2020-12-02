c4e9e34898ddbc3f949680056d344a521faa0b84
Demian Ferreiro <epidemian@gmail.com>
2020-12-02 12:13:31 -0300

    Day 1 solution

---
+++ day1.rb
@@ -0,0 +1,7 @@
+nums = File.read('day1-input').split.map(&:to_i)
+
+a, b = nums.combination(2).find { |a, b| a + b == 2020 }
+puts a * b
+
+a, b, c = nums.combination(3).find { |a, b, c| a + b + c == 2020 }
+puts a * b * c
