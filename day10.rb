f3bdeceb722f4b090be9fbf41a14b3aaf373f3be
Demian Ferreiro <epidemian@gmail.com>
2020-12-10 18:50:07 -0300

    Day 10 solution

---
+++ day10.rb
@@ -0,0 +1,16 @@
+joltages = [0] + File.read('day10-input').split.map(&:to_i).sort
+
+diffs = joltages.map.with_index { |j, i|
+  (joltages[i + 1] || joltages.last + 3) - j
+}
+puts diffs.count(1) * diffs.count(3)
+
+ways = {0 => 1}
+ways.default = 0
+joltages.each do |j|
+  ways[j + 1] += ways[j]
+  ways[j + 2] += ways[j]
+  ways[j + 3] += ways[j]
+end
+
+puts ways[joltages.last + 3]
