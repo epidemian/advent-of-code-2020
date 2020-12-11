8c2ea91e14025cfc07f76460b12d1a7ae160f388
Demian Ferreiro <epidemian@gmail.com>
2020-12-11 03:57:46 -0300

    Refactor

--- day11.rb
+++ day11.rb
@@ -1,19 +1,51 @@
-char_to_sym = {'.' => :floor, 'L' => :empty, '#' => :occupied}
-grid = File.read('day11-input').split.map { |line| line.chars.map(&char_to_sym) }
-DIRECTIONS = [
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, -1],
-  [0, 1],
-  [1, -1],
-  [1, 0],
-  [1, 1],
-]
+at_exit do
+  char_to_sym = {'.' => :floor, 'L' => :empty, '#' => :occupied}
+  grid = File.read('day11-input').split.map { |l| l.chars.map(&char_to_sym) }
 
-def at(grid, x, y)
-  grid[y][x] if y.between?(0, grid.size - 1) && x.between?(0, grid[0].size - 1)
+  stable_grid_1 = simulate_till_stable(grid,
+    count_occupied: method(:adjacents_occupied),
+    leave_threshold: 4
+  )
+  puts total_occupied(stable_grid_1)
+
+  stable_grid_2 = simulate_till_stable(grid,
+    count_occupied: method(:visible_occupied),
+    leave_threshold: 5
+  )
+  puts total_occupied(stable_grid_2)
 end
+
+def simulate_till_stable(grid, **update_args)
+  while (next_grid = update_grid(grid, **update_args)) != grid
+    grid = next_grid
+  end
+  grid
+end
+
+def update_grid(grid, count_occupied:, leave_threshold:)
+  grid.map.with_index { |line, y|
+    line.map.with_index { |seat, x|
+      close_occupied = count_occupied.call(grid, x, y)
+      if seat == :empty && close_occupied.zero?
+        :occupied
+      elsif seat == :occupied && close_occupied >= leave_threshold
+        :empty
+      else
+        seat
+      end
+    }
+  }
+end
+
+def total_occupied(grid)
+  grid.sum { |line| line.count(:occupied) }
+end
+
+DIRECTIONS = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1],           [0, 1],
+  [1, -1],  [1, 0],  [1, 1],
+]
 
 def adjacents_occupied(grid, x, y)
   DIRECTIONS.count { |dx, dy| at(grid, x + dx, y + dy) == :occupied }
@@ -32,40 +64,7 @@ def visible_from(grid, x, y, dx, dy)
   end
 end
 
-def update_grid(grid, count_occupied:, leave_threshold:)
-  grid.map.with_index { |line, y|
-    line.map.with_index { |seat, x|
-      close_occupied = count_occupied.call(grid, x, y)
-      if seat == :empty && close_occupied.zero?
-        :occupied
-      elsif seat == :occupied && close_occupied >= leave_threshold
-        :empty
-      else
-        seat
-      end
-    }
-  }
+def at(grid, x, y)
+  grid[y][x] if y.between?(0, grid.size - 1) && x.between?(0, grid[0].size - 1)
 end
 
-def simulate_till_stable(grid, **update_args)
-  while (next_grid = update_grid(grid, **update_args)) != grid
-    grid = next_grid
-  end
-  grid
-end
-
-def total_occupied(grid)
-  grid.sum { |line| line.count(:occupied) }
-end
-
-stable_grid_1 = simulate_till_stable(grid,
-  count_occupied: method(:adjacents_occupied),
-  leave_threshold: 4
-)
-puts total_occupied(stable_grid_1)
-
-stable_grid_2 = simulate_till_stable(grid,
-  count_occupied: method(:visible_occupied),
-  leave_threshold: 5
-)
-puts total_occupied(stable_grid_2)
